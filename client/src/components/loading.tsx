f7d490de97fb22bf584fbf0d844a96498a87d5d9
notlostagain <51070831+notlostagain@users.noreply.github.com>
2025-05-07 06:29:14 +0000

    fix: update footer copyright year and loading component color

--- client/src/components/loading.tsx
+++ client/src/components/loading.tsx
@@ -5,7 +5,7 @@ export function Waiting({ for: wait, children }: { for?: any, children?: React.R
         <>
             {!wait ?
                 <div className="w-full h-96 flex flex-col justify-center items-center mb-8 ani-show-fast">
-                    <ReactLoading type="cylon" color="#FC466B" />
+                    <ReactLoading type="cylon" color="#f7d0a6" />
                 </div>
                 : children}
         </>
